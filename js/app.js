4fe2a07707a933e6620c972c7c82539c20df8018
Mustafa Hussien <mustafa.hussien100@gmail.com>
2022-04-20 04:19:45 +0200

    function isInViewport amendments

--- js/app.js
+++ js/app.js
@@ -37,9 +37,7 @@ function isInViewport(element) {
   const rect = element.getBoundingClientRect();
   return (
       rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      (rect.bottom / window.innerHeight >= 0.50) || (rect.bottom / document.documentElement.clientHeight >= 0.50)
   );
 }
 
@@ -64,10 +62,14 @@ navList.appendChild(fragment);
 // Check if the section is in the view port to add active class
 window.addEventListener('scroll', function(){
   sections.forEach(section => {
+    const dataNav = section.getAttribute('data-nav');
+    const link = navList.querySelector(`[data-nav="${dataNav}"]`);
     if (isInViewport(section)){
       section.classList.add('your-active-class');
+      link.classList.add('active');
     } else {
       section.classList.remove('your-active-class');
+      link.classList.remove('active');
     }
   })
 } )
